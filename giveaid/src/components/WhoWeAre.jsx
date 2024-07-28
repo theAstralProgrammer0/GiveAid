2292f8393cc20cd3b93884854d3dc68e1f7e838a
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-07-28 14:52:26 +0100

    edited WhoWeAre title

--- giveaid/src/components/WhoWeAre.jsx
+++ giveaid/src/components/WhoWeAre.jsx
@@ -7,7 +7,7 @@ const WhoWeAre = () => {
   return (
     <section id="who-we-are" className="max-w-4xl mx-auto my-6 bg-palette-hash dark:bg-opacity-8 dark:text-palette-text rounded-lg">
       <div className="flex justify-center m-6">
-        <h2 className="w-[62%] text-[71px] font-semibold rounded-lg text-center text-palette-major dark:text-palette-text">Who We Are</h2>
+        <h2 className="w-[62%] text-[71px] font-semibold rounded-lg text-center text-palette-major dark:text-palette-text">GiveAid's All About Giving Seamlessly</h2>
       </div>
       <Boxes />
       <EightCharitableCauses /> {/* Add Eight Charitable Causes component */}
